f6e28e9c78cfa0b181c22e00e5cb4cfa3b5599f7
Blanxxty <Mvtvr21@yandex.ru>
2024-07-23 22:46:53 +0300

    wbView

--- InstaType_PetProject/InstaType/Auth/WebViewViewController.swift
+++ InstaType_PetProject/InstaType/Auth/WebViewViewController.swift
@@ -26,20 +26,82 @@ class WebViewViewController: UIViewController {
         let webView = WKWebView(frame: .zero)
         webView.backgroundColor = .white
         webView.navigationDelegate = self
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress),
+                            options: .new, 
+                            context: nil)
         webView.translatesAutoresizingMaskIntoConstraints = false
         return webView
+    }()
+    
+    private lazy var progressView: UIProgressView = {
+        let progress = UIProgressView()
+        progress.progressTintColor = .red
+        progress.trackTintColor = .green
+        
+        progress.progress = 0.1
+        progress.translatesAutoresizingMaskIntoConstraints = false
+        return progress
+    }()
+    
+    private lazy var backButton: UIButton = {
+       let button = UIButton()
+        button.setImage(UIImage(named: "Backward"), for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         setupWebView()
+        updateProgress()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        removeWebViewObserver()
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, 
+                               of object: Any?,
+                               change: [NSKeyValueChangeKey : Any]?,
+                               context: UnsafeMutableRawPointer?) {
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
+            updateProgress()
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+    
+    private func updateProgress() {
+        progressView.progress = Float(webView.estimatedProgress)
+        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+    }
+    
+    private func removeWebViewObserver() {
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
+    }
+    
+    @objc
+    private func backButtonTapped() {
+        dismiss(animated: true)
     }
     
     private func setupUI() {
+        let backButton = UIBarButtonItem(customView: backButton)
+        navigationItem.leftBarButtonItem = backButton
+        
         view.addSubview(webView)
+        view.addSubview(progressView)
         
         NSLayoutConstraint.activate([
+            progressView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            
             webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
@@ -72,7 +134,7 @@ extension WebViewViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView,
                  decidePolicyFor navigationAction: WKNavigationAction,
                  decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        if let code = code(from: navigationAction) {
+        if code(from: navigationAction) != nil {
             //TODO: procces code
             decisionHandler(.cancel)
         } else {
